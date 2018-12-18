a27b62f2867cb21de2c80b4f4643cc6e825bfd29
Vladimir Utratenko <vutratenko@wiley.com>
2018-12-18 18:44:27 +0300

    Add polling

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ updater = Updater(token=bot_properties.TG_BOT_TOKEN)
 dispatcher = updater.dispatcher
 dispatcher.add_handler()
 
-
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+updater.start_polling()
