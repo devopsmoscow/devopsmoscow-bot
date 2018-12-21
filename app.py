b2c1ce32463d97394f555e66bd91a5b5048a61e8
Vladimir Utratenko <vutratenko@wiley.com>
2018-12-21 18:25:42 +0300

    Trying to get new users

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from devopsmoscow_bot import bot_properties
-from telegram.ext import Updater, CommandHandler
-from telegram import Chat
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from devopsmoscow_bot.bot.spammer import Spammer
+import logging
 
 updater = Updater(token=bot_properties.TG_BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 40})
 dispatcher = updater.dispatcher
-
-
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
 def get_users(bot, update):
@@ -15,10 +12,12 @@ def get_users(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=upd)
 
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', Spammer.start)
 users_handler = CommandHandler('users', get_users)
+add_group_handler = MessageHandler(callback=Spammer.add_group, filters=Spammer.NewMember())
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(users_handler)
+dispatcher.add_handler(add_group_handler)
 updater.start_polling()
 updater.idle()
 
---
+++ devopsmoscow_bot/bot/spammer.py
@@ -0,0 +1,31 @@
+import logging
+
+from telegram.ext import BaseFilter
+
+from devopsmoscow_bot import bot_properties
+
+
+class Spammer:
+
+    def __init__(self):
+        self.data = []
+
+    def start(self, bot, update):
+        if update.message.chat_id is not bot_properties.DEVOPS_SUPERGROUP_DETAILS['id']:
+            bot.send_message(chat_id=update.message.chat_id, text="This bot is under construction. You'll get an "
+                                                                  "additional notification once it will be done.")
+        else:
+            pass
+
+    class NewMember(BaseFilter):
+        def filter(self, message):
+            if not message.new_chat_members:
+                return False
+            return True
+
+    def add_group(self, bot, update):
+        for members in update.message.new_chat_members:
+            logger = logging.getLogger("deopsmoscow_bot.bot.spammer.Spammer.add_group")
+            logger.debug("Got " + members.username + " as new user!")
+            logger.debug(members)
+            # bot.send_message(update.message.chat_id, text="{username} add group".format(username=members.username))
