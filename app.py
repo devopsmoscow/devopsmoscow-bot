9cedd2acaa0d028807b52ec1760ad222c1437d07
Vladimir Utratenko <vutratenko@wiley.com>
2018-12-21 16:56:32 +0300

    Trying to get users

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from devopsmoscow_bot import bot_properties
 from telegram.ext import Updater, CommandHandler
-
+from telegram import Chat
 
 updater = Updater(token=bot_properties.TG_BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 40})
 dispatcher = updater.dispatcher
@@ -11,10 +11,8 @@ def start(bot, update):
 
 
 def get_users(bot, update):
-    updates = updater.bot.get_updates()
-    bot.send_message(chat_id=update.message.chat_id, text=[u.message.text for u in updates])
-    # chat = Chat(id=bot_properties.DEVOPS_SUPERGROUP_DETAILS[""])
-    # pass
+    upd = bot.get_chat_members_count(chat_id=bot_properties.DEVOPS_SUPERGROUP_DETAILS["id"])
+    bot.send_message(chat_id=update.message.chat_id, text=upd)
 
 
 start_handler = CommandHandler('start', start)
--- devopsmoscow_bot/bot_properties.py
+++ devopsmoscow_bot/bot_properties.py
@@ -8,5 +8,6 @@ TG_BOT_TOKEN = os.environ['TG_KEY']
 DEVOPS_SUPERGROUP_DETAILS = {
     "group_title": "DevOps Moscow",
     "username": "devopsmoscow",
-    "type": "supergroup"
+    "type": "supergroup",
+    "id": -1001217118773
 }
