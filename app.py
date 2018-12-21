4bf644cff412b62df1d2b57bc48a8934e3c19a94
Vladimir Utratenko <vutratenko@wiley.com>
2018-12-21 15:53:30 +0300

    Continue diving into API

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import telegram
 from devopsmoscow_bot import bot_properties
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler
 
-bot = telegram.Bot(token=bot_properties.TG_BOT_TOKEN)
-print(bot.get_me())
 
 updater = Updater(token=bot_properties.TG_BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 40})
 dispatcher = updater.dispatcher
@@ -14,7 +10,17 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
+def get_users(bot, update):
+    updates = updater.bot.get_updates()
+    bot.send_message(chat_id=update.message.chat_id, text=[u.message.text for u in updates])
+    # chat = Chat(id=bot_properties.DEVOPS_SUPERGROUP_DETAILS[""])
+    # pass
+
+
 start_handler = CommandHandler('start', start)
+users_handler = CommandHandler('users', get_users)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(users_handler)
 updater.start_polling()
 updater.idle()
+
--- devopsmoscow_bot/bot_properties.py
+++ devopsmoscow_bot/bot_properties.py
@@ -4,4 +4,9 @@ import os
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-TG_BOT_TOKEN=os.environ['TG_KEY']
+TG_BOT_TOKEN = os.environ['TG_KEY']
+DEVOPS_SUPERGROUP_DETAILS = {
+    "group_title": "DevOps Moscow",
+    "username": "devopsmoscow",
+    "type": "supergroup"
+}
