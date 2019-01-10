13e392076e40f671b4f2d8c8ee3716d438084387
Vladimir Utratenko <vutratenko@wiley.com>
2019-01-10 22:58:04 +0300

    Clean redundant code

--- devopsmoscow_bot/bot/spammer.py
+++ devopsmoscow_bot/bot/spammer.py
@@ -58,11 +58,7 @@ class Spammer:
             request.query = update.message.text
             response_json = json.loads(request.getresponse().read().decode('utf-8'))
             response = response_json['result']['fulfillment']['speech']
-            if response_json:
-                try:
-                    logger.debug(str(response_json))
-                except Exception as e:
-                    pass
+            if response:
                 bot.send_message(chat_id=update.message.chat_id, text=response)
             elif not response and update.message.chat_id == bot_properties.GROUP_CHAT_ID:
                 pass
